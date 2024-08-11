371c25804e9850398e1f67260d7cf053fcba8655
kratika <146112064+kratika-5@users.noreply.github.com>
2024-08-11 19:31:52 +0530

    Time: 1 ms (98.88%), Space: 41.7 MB (22.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        
+
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                default:
+                    return false; // Invalid character
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
+
+    
