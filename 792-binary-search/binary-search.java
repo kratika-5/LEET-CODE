ebaf61245a6ea91a3c88922659bd3cc3a64ee12c
kratika <146112064+kratika-5@users.noreply.github.com>
2024-10-10 09:27:08 -0700

    Time: 1 ms (11.05%) | Memory: 45.5 MB (63.08%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int flag=0;
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+             return i;
+        
+        }
+            
+                return -1;
+                
+               
+    
+        
+}
+}
