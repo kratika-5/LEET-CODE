ee082b0a66025d35159a025d5e81d1054ae70a15
kratika <146112064+kratika-5@users.noreply.github.com>
2025-05-30 00:08:27 +0530

    Time: 2 ms (75.22%) | Memory: 61.9 MB (41.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,19 @@
-import java.util.*;
 class Solution {
     public int maxProfit(int[] prices) {
-     int mini=prices[0];
-        int profit=0;
-        int cost=0;
-        for(int i=0;i<prices.length;i++)
-        {
-            cost=prices[i]-mini;
-            profit=Math.max(profit, cost);
-            mini=Math.min(mini, prices[i]);
+         int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; // update min so far
+            } else {
+                int profit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
         }
-    
-    return profit;
+
+        return maxProfit;
     }
 }
