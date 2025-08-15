e9f07c4e0acfe2594f52e8381d3e566813af53bc
kratika <146112064+kratika-5@users.noreply.github.com>
2025-08-16 02:30:50 +0530

    Time: 3 ms (91.99%) | Memory: 44.7 MB (7.06%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[]=new int [nums1.length];
-      for (int i = 0; i < nums1.length; i++) {
-    int k = nums1[i];
-    boolean found = false;
-    for (int j = 0; j < nums2.length; j++) {
-        if (nums2[j] == k) {
-            found = true;
-        }
-        if (found && nums2[j] > k) {
-            arr[i] = nums2[j];
-            break;
-        }
-    }
-    if (!found || arr[i] == 0) {
-        arr[i] = -1;
-    }
-}
+        
+       Map<Integer,Integer>nge = new HashMap<>();
+       Stack<Integer>st = new Stack<>();
 
-        return arr;
+       for(int i = nums2.length-1;i>=0;i--)
+       {
+         int num=nums2[i];
+
+         while(!st.isEmpty()&&st.peek()<=num)
+         {
+            st.pop();
+         }
+         nge.put(num,st.isEmpty() ? -1:st.peek());
+         st.push(num);
+       }
+       int result[]= new int[nums1.length];
+       for(int i =0;i<nums1.length;i++)
+       {
+         result[i]=nge.get(nums1[i]);
+       }
+       return result;
     }
 }
